643ad42f7630280980f1cf1000337c02615766ae
Michael-Harber <129912256+Michael-Harber@users.noreply.github.com>
2024-03-12 17:26:08 +0000

    Updated views

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,5 +3,10 @@ from . import views
 
 urlpatterns = [
   path("",views.home, name="home"),
-  path("about",views.about, name="about"),
+  path("about/",views.about, name="about"),
+  path('function/', views.function_view, name='function_view'),
+  path('contact/', views.contact, name='contact'),
+  path('class/', views.ClassView.as_view(), name='class_view'),
+  path('gallery/', views.gallery.as_view(), name='gallery'),
 ]
+
